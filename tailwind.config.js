2153f114fc2935dc29f8aba6558d660e98c8f5e3
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-01 22:10:56 +0800

    :lipstick: add daisy ui style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'class',
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [require('@tailwindcss/forms'), require('kutty')],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  daisyui: {
+    themes: ["cyberpunk"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
