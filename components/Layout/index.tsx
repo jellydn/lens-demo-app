d29204869ef6176b6f795a12ce1ee7f53ea2c2f1
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-01 23:30:14 +0800

    refactor: use rainbowkit UI

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,28 +1,50 @@
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
 import { Toaster } from 'react-hot-toast';
+import { WagmiConfig, chain, configureChains, createClient } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
 
-import Header from "../Header";
-import Authentication from "../Authentication";
+import Authentication from '../Authentication';
+import Header from '../Header';
+
+const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon],
+    [publicProvider()],
+);
+
+const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains,
+});
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+});
 
 const Layout: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
-  <div>
-    <div className="flex flex-row justify-around mx-auto w-full">
-      <Header
-        links={[
-          {
-            title: "Contact",
-            url: "/contact",
-          },
-          {
-            title: "Lens Profile",
-            url: "/profile",
-          },
-        ]}
-      />
-      <Authentication />
-    </div>
-    {children}
-    <Toaster />
-  </div>
+    <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+            <div className="flex flex-row justify-around w-full mx-auto">
+                <Header
+                    links={[
+                        {
+                            title: 'Contact',
+                            url: '/contact',
+                        },
+                        {
+                            title: 'Lens Profile',
+                            url: '/profile',
+                        },
+                    ]}
+                />
+                <Authentication />
+            </div>
+            {children}
+            <Toaster />
+        </RainbowKitProvider>
+    </WagmiConfig>
 );
 
 export default Layout;
